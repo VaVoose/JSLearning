59bbfb0fa59745a5251975ac785360d3fffa360a
Ferrante <daf5-31@sbcglobal.net>
2020-07-07 10:24:48 -0400

    Added Animal and Rabbit class

--- jslearning6.js
+++ jslearning6.js
@@ -51,6 +51,61 @@ class Person {
     }
 }
 
+class Animal {
+    constructor(name){
+        this.speed = 0;
+        this.name = name;
+    }
+
+    stop(){
+        alert(`${this.name} has stopped`);
+        this.speed = 0;
+    }
+
+    run(speed){
+        this.speed = speed;
+        alert(`${this.name} is running ${this.speed}f/s`);
+    }
+}
+
+class Rabbit extends Animal {
+    //Overridden constructors need to use the super(...args) call in order to work
+    constructor(name, earLength){
+        super(name);
+        this.earLength = earLength;
+    }
+
+    hide(){
+        alert(`${this.name} has hid`);
+    }
+
+    //overridden function
+    stop(){
+        super.stop();
+        this.hide();
+    }
+
+    get earLength(){
+        return this._earLength;
+    }
+
+    set earLength(earLength) {
+        this._earLength = earLength;
+    }
+}
+
+class Article {
+    constructor (title, date){
+        this.title = title;
+        this.date = date;
+    }
+
+    //Example of static method, particular example is of a Factory type
+    static createTodaysArticle(){
+        return new this("Todays Digest", new Date());
+    }
+}
+
 let p1 = new Person();
 let p2 = new Person("Dominic");
 //this doesn't require the arrow notation because its not used in call back
@@ -59,4 +114,15 @@ p2.computedFunctionName();
 
 //this however doesn't work unless the sayHi is used in arrow notation
 setTimeout(p2.sayHi, 1000);
+
+let a1 = new Animal("Josh");
+a1.stop();
+
+let r1 = new Rabbit("Rabbito", 4);
+r1.stop();
+
+alert(r1.earLength);
+
+let newArticle = Article.createTodaysArticle();
+alert(`${newArticle.title} came out ${newArticle.date}`);
 
